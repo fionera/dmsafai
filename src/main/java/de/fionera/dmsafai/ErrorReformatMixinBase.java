afe8f2383a38e37610511b01867074476b5e5373
Fionera <fionera@fionera.de>
2024-08-24 02:51:13 +0200

    fix access

--- src/main/java/de/fionera/dmsafai/ErrorReformatMixinBase.java
+++ src/main/java/de/fionera/dmsafai/ErrorReformatMixinBase.java
@@ -2,11 +2,11 @@ package de.fionera.dmsafai;
 
 
 public abstract class ErrorReformatMixinBase {
-    protected static Object[] cleanArgs(Object o1, Object o2) {
+    public static Object[] cleanArgs(Object o1, Object o2) {
         return cleanArgs(new Object[]{o1, o2});
     }
 
-    protected static Object[] cleanArgs(Object[] o) {
+    public static Object[] cleanArgs(Object[] o) {
         for (int i = 0; i < o.length; i++) {
             if (o[i] instanceof Exception) {
                 o[i] = ((Exception) o[i]).getMessage();
